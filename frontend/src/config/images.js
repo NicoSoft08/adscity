442f42a47100308bbf0e404d5930748173dd53da
Nicolas Peniel <135339436+NicoSoft08@users.noreply.github.com>
2024-12-19 01:25:25 +0300

    Add files via upload

---
+++ frontend/src/config/images.js
@@ -0,0 +1,21 @@
+export const IconAvatar = require('../imgs/user-avatar.png');
+export const IconProfil = require('../imgs/profil-pic.png');
+export const IconNotAllowed = require('../imgs/not-allowed.png');
+export const IconRegister = require('../imgs/register.png');
+export const IconCreateAd = require('../imgs/btn-text.png');
+export const IconCreateAdButton = require('../imgs/btn.png');
+export const IconCover = require('../imgs/cover.jpg');
+export const Icon404 = require('../imgs/404.png');
+export const IconPrivacy = require('../imgs/privacy.png');
+export const IconDataProcess = require('../imgs/data-process.png');
+export const IconAdsRules = require('../imgs/ads-rules.png');
+
+
+// START ALL CATEGORIES
+export const alimentation = require('../imgs/all-categories/alimentation/nutrition.png');
+export const electronique = require('../imgs/all-categories/electronique/appareil-electronique.png');
+export const mode_et_beaute = require('../imgs/all-categories/mode-et-beaute/produits-de-beaute.png');
+export const services = require('../imgs/all-categories/les-services/services.png');
+
+
+
---
+++ frontend/src/config/index.js
@@ -0,0 +1,22 @@
+import { IconAvatar, IconCover } from './images';
+
+export const defaultProfilURL = IconAvatar;
+export const defaultCoverURL = IconCover;
+
+export const IconTransport = require('../assets/svg/transport.png');
+export const IconImmobilier = require('../assets/svg/immobilier.png');
+export const IconElectronics = require('../assets/svg/electronics.png');
+export const IconFashion = require('../assets/svg/fashion.png');
+
+export const IconBadgeVerified = require('../assets/icons/adscity-verified-badge.png');
+export const IconVerifiedModified = require('../assets/icons/adscity-verified-modified.png');
+
+const mediaSocio = {
+    facebook: require('../assets/social-media/facebook.png'),
+    instagram: require('../assets/social-media/instagram.png'),
+    whatsapp: require('../assets/social-media/whatsapp.png'),
+    youtube: require('../assets/social-media/youtube.png'),
+    website: '',
+}
+
+export { mediaSocio };

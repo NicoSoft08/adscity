24803d28926c0edb995dade0dc7ff319f8c79d8a
Nicolas Peniel <135339436+NicoSoft08@users.noreply.github.com>
2025-01-17 00:41:52 +0300

    Add files via upload

--- frontend/src/components/plan-item/PlanItem.jsx
+++ frontend/src/components/plan-item/PlanItem.jsx
@@ -9,7 +9,7 @@ export default function PlanItem({ plan, id, onSelect, selectedPlan, setSelected
         setSelectedPlan(id);
     };
 
-    const isParticulierPlan = plan?.name.toLowerCase() === "particulier" || plan?.name.toLowerCase() === "professionnel";
+    const isParticulierPlan = plan?.name.toLowerCase() === "particulier";
 
     return (
         <div
